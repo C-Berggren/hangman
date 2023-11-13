50728f0fad0cf6d0c91ac0e0566680cfe8d868ff
C-Berggren <>
2023-11-13 12:47:31 +0000

    Added username, secret world list and counters

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Player's username
+user_name = input("Your name: ")
+print(f"Welcome {user_name}! Let's play!")
+print("Start playing! Choose your first letter. You have 7 tries before x_x")
+
+# Program will choose one of the words from the list
+word_list = ["secret", "human", "baby", "dog", "brilliant", "joyful", "wonder", "kitten", "friend"]
+
+# For dev
+secret_word = "brunch"
+
+# Number of guesses
+guesses = ""
+
+# Counter of turns
+turns = 7
+
+while turns > 0:
+    # failed counter
+    failed = 0
