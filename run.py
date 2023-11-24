ca0105dd8b7b4f69cb1f9c250ba83ac412bf6622
C-Berggren <>
2023-11-24 11:05:06 +0000

    adding false print statements

--- run.py
+++ run.py
@@ -19,9 +19,27 @@ def play(word):
     tries = 7
     # Player's username
     user_name = input("Your name: ")
+    # Printing the Start gameplay
     print(f"Welcome {user_name}! Let's play!")
     print(display_hangman(tries))
     print(word_completion)
     print("\n")
+
+    # while loop to check your guesses are alphabetial and single or word
     while not guessed and tries > 0:
-        guess = input("Start playing! Choose your first letter. You have 7 tries before x_x")
+        guess = input("Start playing! Choose your first letter. You have 7 tries before x_x").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed this letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word")
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed this word", guess)
+            elif guess not in word:
+                print(guess, "was the wrong word")
+        else:
+            print("Incorrect, please try again")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
