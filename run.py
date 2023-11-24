283ff70e951b3607608bfd1bdd69e8af7038b3be
C-Berggren <>
2023-11-24 10:17:19 +0000

    Deleting most of the code to redo it.

--- run.py
+++ run.py
@@ -1,37 +1,6 @@
+import random
+
 # Player's username
 user_name = input("Your name: ")
 print(f"Welcome {user_name}! Let's play!")
 print("Start playing! Choose your first letter. You have 7 tries before x_x")
-
-# Player guesses
-guess = input("Guess a letter or word: ")
-
-# Program will choose one of the words from the list
-word_list = ["secret", "human", "baby", "dog", "brilliant", "joyful", "wonder", "kitten", "friend"]
-
-# For dev
-secret_word = "brunch"
-
-# Number of guesses
-guesses = ""
-
-# Counter of turns
-turns = 7
-
-# while loop to check number of tries versus number of turns
-while turns > 0:
-    # failed counter
-    failed = 0
-
-    if char in guesses:
-        print(char,end="")
-
-    else:
-        print("_",end="")
-        failed += 1
-
-# When you win
-    if failed == 0:        
-        print ("You won")
-    # exit the script
-        break 
