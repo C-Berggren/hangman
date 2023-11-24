5e4947a95a6f4bfb48474ed776283a0cd1ddf01a
C-Berggren <>
2023-11-24 12:45:58 +0000

    Redoing the code again, keeping list

--- run.py
+++ run.py
@@ -104,50 +104,8 @@ word_list = [
     'two'
 ]
 
-
-# Defined word to be chosen from wordlist and making the words uppercase
-def get_word():
-    word = random.choice(word_list)
-    return word.upper()
-
-
-# Defining the play function
-def play(word):
-    word_completion = "_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    tries = 7
-    # Player's username
-    user_name = input("Your name: ")
-    # Printing the Start gameplay
-    print(f"Welcome {user_name}! Let's play!")
-    print(word_completion)
-    print("\n")
-
-    # while loop to check your guesses are alphabetial and single or word
-    while not guessed and tries > 0:
-        guess = input("Start playing! Choose your first letter: ")
-        print("You have 7 tries before x_x")
-        if len(guess) == 1 and guess.isalpha():
-            if guess in guessed_letters:
-                print("You already guessed this letter", guess)
-            elif guess not in word:
-                print(guess, "is not in the word")
-                tries -= 1
-                guessed_letters.append(guess)
-            else:
-                print(guess, "is in the word. Well done!")
-                guessed_letters.append(guess)
-                if "_" not in word_completion:
-                    guessed = True
-                    print(word, f"was correct {user_name}! You survived!")
-        elif len(guess) == len(word) and guess.isalpha():
-            if guess in guessed_words:
-                print("You already guessed this word", guess)
-            elif guess not in word:
-                print(guess, "was the wrong word")
-        else:
-            print("Incorrect, please try again")
-        print(word_completion)
-        print("\n")
+# Player's username
+user_name = input("Your name: ")
+# Printing the Start gameplay
+print(f"Welcome {user_name}! Let's play!")
+    
